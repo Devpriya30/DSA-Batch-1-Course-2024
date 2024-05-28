69be7ef54104b6f08849d26db190d8356be62d6d
santosh yeswanth <32554857+sant0143@users.noreply.github.com>
2024-05-28 14:25:33 +0530

    Create Find the Highest Altitude.cpp

---
+++ Dundu Santosh Yeshwanth/Arrays/Find the Highest Altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int current = 0,max1=0;
+        for(int i=0;i<gain.size();i++){
+            current += gain[i];
+            max1 = max(max1,current);
+        }
+        return max1;
+    }
+};
