bfebbbf101dbb04952a04b33f586ef5be3034239
santosh yeswanth <32554857+sant0143@users.noreply.github.com>
2024-05-28 17:09:18 +0530

    Create 1480. Running Sum of 1D Array.cpp

---
+++ Dundu Santosh Yeshwanth/Arrays/1480. Running Sum of 1D Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>res;
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
+//Time Complexity - O(N)
+//Space Complexity - O(N)
