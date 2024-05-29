4e5ef43e7061b6d684c7c947a9f6cb66e2826a95
santosh yeswanth <32554857+sant0143@users.noreply.github.com>
2024-05-29 13:06:10 +0530

    Create 169-Majority Element.cpp

---
+++ Dundu Santosh Yeshwanth/Arrays/169-Majority Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(auto ele:nums){
+            mp[ele]++;
+        }
+        n = n/2;
+        for(auto x:mp){
+            if(x.second>n){
+                return x.first;
+            }
+        }
+	return 0;
+    }
+};
+//Time Complexity - O(N)
+//Space Complexity - O(1)
