114b76e7f4c46b1f012366f503ac1ab3020edee3
vishakha2406 <167342888+vishakha2406@users.noreply.github.com>
2024-06-06 20:29:11 +0530

    Create 2373. Largest Local Values in a Matrix.java

---
+++ Vishakha Mishra /ARRAY/2373. Largest Local Values in a Matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] arr = new int[n-2][n-2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int max = 0;
+                for (int k = i; k < i + 3; k++) {
+                    for (int l = j; l < j + 3; l++) {
+                        if (grid[k][l] > max) {
+                            max = grid[k][l];
+                        }
+                    }
+                }
+                arr[i][j] = max;
+            }
+        }
+        return arr;
+    }
+}
