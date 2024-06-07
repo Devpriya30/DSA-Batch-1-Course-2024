a4169b917cdfc0e87f8227febf8017da63ef7826
AashutoshKS <aashutoshkumarsingh71@gmail.com>
2024-06-07 17:04:04 +0530

    Create 24 : 53. Maximum subarray.cpp

---
+++ Aashutosh Kumar sigh/ARRAY/24 : 53. Maximum subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxsum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            maxsum=max(maxsum,sum);
+            if(sum<0)
+            sum=0;
+        }
+        return maxsum;
+    }
+};
+
+TIME COMPLEXITY : O(n)
+SPACE COMPLEXITY : O(1)
