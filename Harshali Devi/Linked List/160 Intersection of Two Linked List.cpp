2729a583f90c70ed87d03371c9123edb08811ceb
HarshaliD <69595118+HarshaliD@users.noreply.github.com>
2024-06-11 20:44:28 +0530

    Create 160 Intersection of Two Linked List.cpp

---
+++ Harshali Devi/Linked List/160 Intersection of Two Linked List.cpp
@@ -0,0 +1,38 @@
+//Day 11
+//Question 58
+//https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+
+//Optimized Solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while(temp1!=temp2){
+            if(temp1!=NULL){
+                temp1=temp1->next;
+            }
+            else{
+                temp1=headB;
+            }
+            if(temp2!=NULL){
+                temp2=temp2->next;
+            }
+            else{
+                temp2=headA;
+            }
+        }
+        return temp1;
+    }
+};
