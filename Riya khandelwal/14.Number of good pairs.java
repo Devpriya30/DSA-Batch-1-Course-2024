49bf9a50cc648c5df68ccf13c197996e7d3c79c5
Riya130305 <riyakhandelwal4776@gmail.com>
2024-06-11 11:36:49 +0530

    Create 14.Number of good pairs.java

---
+++ Riya khandelwal/14.Number of good pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i] == nums[j] && i<j)
+                {
+                  c++;
+                }
+            }
+        }
+        return c;
+        
+    }
+}
+
+//T.C = O(n^2)
+
+//S.C = O(1)
