4362295c830292c4ded6f5ad6138c3d4607ac584
vishakha2406 <167342888+vishakha2406@users.noreply.github.com>
2024-06-19 19:21:42 +0530

    Create 20. Valid Parentheses.java

---
+++ Vishakha Mishra /STACK/20. Valid Parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+           if(c=='('){
+            stack.push(')');
+           }
+           else if(c=='{'){
+            stack.push('}');
+           }
+           else if(c=='['){
+            stack.push(']');
+           }
+           else{
+            if(stack.isEmpty() || c!=stack.peek()){
+                return false;
+            }
+            stack.pop();
+           }
+        }
+        return stack.isEmpty();
+    }
+}
