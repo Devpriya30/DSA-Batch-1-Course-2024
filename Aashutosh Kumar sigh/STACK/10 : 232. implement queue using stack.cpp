dcfca3beb314fabc94d4bf6c58b3ddc6e6373b4a
AashutoshKS <aashutoshkumarsingh71@gmail.com>
2024-06-21 11:37:56 +0530

    Create 10 : 232. implement queue using stack.cpp

---
+++ Aashutosh Kumar sigh/STACK/10 : 232. implement queue using stack.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+public:
+stack<int>s1;
+stack<int>s2;
+int front;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(s1.empty()){
+            front=x;
+        }
+        s1.push(x);
+    }
+
+    int pop() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            int top=s2.top();
+            s2.pop();
+            return top;
+        }
+        int top=s2.top();
+        s2.pop();
+        return top;
+    }
+
+    int peek() {
+        if(s2.empty()){
+            return front;
+        }
+        else{
+            return s2.top();
+        }
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+
+
