9739891c58dd339cf6c6d7f702f163bba3824e02
divyansh-2707 <165201502+divyansh-2707@users.noreply.github.com>
2024-06-22 01:38:26 +0530

    Create Time Needed to Buy Tickets.cpp

---
+++ Divyansh Singh/Stack/Time Needed to Buy Tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+     int n=tickets.size();
+     int time=0;
+     for(int i=0;i<n;i++){
+        if(i<=k){
+           time+=min(tickets[i],tickets[k]);
+        }
+        else{
+           time+=min(tickets[i],tickets[k]-1);
+        }
+     }  
+     return time; 
+    }
+};
