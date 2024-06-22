d9cf1ea69eae04c7193017eb1746a641ace5b0d7
vishakha2406 <167342888+vishakha2406@users.noreply.github.com>
2024-06-22 16:08:16 +0530

    Create 1047. Remove All Adjacent Duplicates In String.java

---
+++ Vishakha Mishra /STACK/1047. Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        int[] res = new int[s.length()];
+
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()==c){
+                stack.pop();
+            }
+            else
+            stack.push(c);    
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
